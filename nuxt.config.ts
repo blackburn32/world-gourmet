1962926f3a33c09e74f8df073966cc4490302249
blackburn32 <alexblackburn32@gmail.com>
2023-10-25 18:14:32 -0700

    Set up for real domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/world-gourmet/',
-    head: {
-      link: [
-        {
-          rel: 'icon',
-          type: 'image/svg+xml',
-          href: '/world-gourmet/favicon.svg',
-        },
-      ],
-    },
-  },
   devtools: { enabled: false },
   modules: ['@nuxt/content'],
   css: ['~/assets/css/main.css'],
