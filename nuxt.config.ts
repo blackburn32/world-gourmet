a9fa671ffb051b5377b78f1e052d5922b6470f39
blackburn32 <alexblackburn32@gmail.com>
2023-10-25 18:19:48 -0700

    Update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: '/favicon.svg',
+        },
+      ],
+    },
+  },
   devtools: { enabled: false },
   modules: ['@nuxt/content'],
   css: ['~/assets/css/main.css'],
