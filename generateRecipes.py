02e636d3fbb22d4de5e3c0ceb4e78b9103de6cbc
blackburn32 <alexblackburn32@gmail.com>
2023-10-28 12:48:42 -0700

    Add factory script for recipes.

---
+++ generateRecipes.py
@@ -0,0 +1,86 @@
+import os
+import openai
+
+openai.api_key = "" # API KEY HERE
+
+messages = [
+    {"role": "system", "content": "You are a helpful web blogger."},
+    {"role": "system", "content": "You are knowledgeable about food and recipes."},
+    {"role": "system", "content": "You always produce correctly formatted markdown in your responses."},
+    {"role": "system", "content": "You exclusively respond with markdown."},
+]
+
+examples = [
+    {"role": "user", "content": "Create a recipe for Apple Pie."},
+    {"role": "assistant", "content": """# Apple Pie
+
+Apple pie is a classic dessert made with a flaky pastry crust and a delicious filling of sweet, tender apples. It's the perfect treat to enjoy during the fall season.
+
+![Apple Pie](https://source.unsplash.com/random/?apple-pie)
+
+**Ingredients**
+- 2 cups of all-purpose flour
+- 1 teaspoon of salt
+- 1 cup of unsalted butter, cold and cubed
+- 6-8 tablespoons of ice-cold water
+- 6-7 medium-sized apples, peeled, cored, and sliced
+- 1 cup of granulated sugar
+- 2 tablespoons of all-purpose flour
+- 1 teaspoon of ground cinnamon
+- 1 teaspoon of ground nutmeg
+- 1 tablespoon of lemon juice
+- 1 tablespoon of unsalted butter, cut into small pieces
+- 1 egg, beaten (for egg wash)
+- 1 tablespoon of granulated sugar (for sprinkling)
+
+**Instructions**
+1. In a large mixing bowl, whisk together the flour and salt.
+2. Add in the cubed butter and mix with a fork or pastry cutter until the mixture resembles coarse crumbs.
+3. Gradually add in the ice-cold water, 1 tablespoon at a time, mixing until the dough comes together.
+4. Divide the dough in half, shape each half into a disc, wrap in plastic wrap, and refrigerate for at least 1 hour.
+5. Preheat the oven to 375F (190C).
+6. In another mixing bowl, combine the sliced apples, granulated sugar, flour, cinnamon, nutmeg, and lemon juice. Toss until the apples are coated evenly.
+7. Roll out one disc of the chilled dough on a lightly floured surface to fit a 9-inch pie dish. Place the rolled dough into the dish and trim any excess.
+8. Fill the pie shell with the apple mixture. Dot the top with small pieces of butter.
+9. Roll out the second disc of dough and place it over the filling. Trim and crimp the edges to seal the pie. Cut slits in the top crust to allow steam to escape.
+10. Brush the top crust with beaten egg and sprinkle with granulated sugar.
+11. Place the pie on a baking sheet to catch any drips, and bake for about 50-60 minutes, or until the crust is golden brown and the filling is bubbling.
+12. Allow the pie to cool for at least 2 hours before serving.
+
+Enjoy your homemade apple pie! It pairs perfectly with a scoop of vanilla ice cream or a dollop of whipped cream."""
+    },
+]
+
+def getRecipeMessageForRecipe(recipe):
+    return {
+        "role": "user",
+        "content": "Create a recipe for " + recipe + "."
+    }
+
+def getMarkdownPageForRecipe(recipe):
+    print("Generating markdown for " + recipe + "...")
+    finalMessages = messages + examples + [getRecipeMessageForRecipe(recipe)]
+    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=finalMessages)
+    return chat_completion.choices[0].message.content
+
+def kebabLowerCase(string):
+    return string.lower().replace(" ", "-")
+
+def writeMarkdownPageToFile(recipe, markdownPage):
+    fileName = kebabLowerCase(recipe)
+    with open("C:/Users/king_/WebstormProjects/world-gourmet/content/recipes/" + fileName + ".md", "w+") as file:
+        file.write(markdownPage)
+
+def generateMarkdownAndWriteForRecipes(recipes):
+    for recipe in recipes:
+        markdownPage = getMarkdownPageForRecipe(recipe)
+        writeMarkdownPageToFile(recipe, markdownPage)
+
+recipes = [
+    "Roasted Asparagus",
+    "Asparagus Stir Fry",
+    "Asparagus Risotto",
+    "Asparagus Soup",
+]
+
+generateMarkdownAndWriteForRecipes(recipes)
